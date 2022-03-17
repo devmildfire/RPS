095f4bdc99701213d42effaabd66293aab17b62c
devmildfire <mildfire@gmail.com>
2022-03-17 21:30:06 +0500

    add game logic is script.js

--- js/script.js
+++ js/script.js
@@ -10,26 +10,59 @@ console.log(`Welcome to the RPS game!`);
 // if the moves are not identical, the game takes users move and 
 // decides wheather it is a win or a lose depending on the game move
 
-const playerMove = prompt(`what is your move? (hint: it's rock, paper or scissors)`).toLowerCase();
 
-const possibleGameMoves = [`rock`, `paper`, `scissors`];
 
-const gameMove = possibleGameMoves[ Math.floor(3*Math.random()) ]; 
-
-// var win = true;
-
-if (playerMove == gameMove) {
-    alert(`your move is ${playerMove} and the game chose ${gameMove}`);
-    alert(`that's a tie!`);
-} else {
-    if (playerMove == `rock` && gameMove == `paper` ||
-        playerMove == `paper` && gameMove == `scissors` || 
-        playerMove == `scissors` && gameMove == `rock`) {
-        // win = false;
-        alert(`your move is ${playerMove} and the game chose ${gameMove}`);
-        alert(`that's a lose!`);
-    } else {
-        alert(`your move is ${playerMove} and the game chose ${gameMove}`);
-        alert(`that's a Win!`);
-    }
+function computerPlay() {
+    const possibleGameMoves = [`rock`, `paper`, `scissors`];
+    return possibleGameMoves[ Math.floor(3*Math.random()) ]; 
 }
+
+function playerSelection() {
+    return prompt(`what is your move? (hint: it's rock, paper or scissors)`).toLowerCase();
+}
+
+function gameRound(playerSelection, computerSelection) {
+
+    if (playerSelection == computerSelection) {
+        return [0, `That's a tie! Your move is ${playerSelection} and the game chose ${computerSelection}`] ;
+    } else {
+        if (playerSelection == `rock` && computerSelection == `paper` ||
+            playerSelection == `paper` && computerSelection == `scissors` || 
+            playerSelection == `scissors` && computerSelection == `rock`) {
+            // win = false;
+            return [1, `That's a lose! Your move is ${playerSelection} and the game chose ${computerSelection}`] ;
+        } else {
+            return [2, `That's a Win! Your move is ${playerSelection} and the game chose ${computerSelection}` ];
+        }
+    }
+
+}
+
+
+function game(numberOfRounds) {
+
+    var userScore = 0;
+    var computerScore = 0;
+    
+
+    for (let i = 0; i < numberOfRounds; i++) {
+        
+        let [status, message]  = gameRound( playerSelection(), computerPlay() ); 
+      
+
+        if (status == 1) {
+            computerScore += 1;
+        } else if (status == 2) {
+            userScore += 1;
+        }
+        
+        alert( message );
+
+    }
+
+    alert( `your score is ${userScore} and computer score is ${computerScore}. 
+    You ${userScore < computerScore ? `lose` : `WIN` }`  );
+
+}
+
+game(3);
